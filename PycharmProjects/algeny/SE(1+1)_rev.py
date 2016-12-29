49930d830f41d63aabab8ae5c91d3c0def972473
kaz94 <katalonka111@gmail.com>
2016-12-29 21:33:30 +0100

    prosta optymalizacja działa

--- PycharmProjects/algeny/SE(1+1)_rev.py
+++ PycharmProjects/algeny/SE(1+1)_rev.py
@@ -2,14 +2,15 @@ import numpy as np
 import random
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from reverberation_time import reverberation_time
 
-x, y = np.array([5., 5.])
+fitness = reverberation_time
+angle = np.array([0.])
+#angle, x, y = np.array([0., 0., 0.])
 
 
-def fitness(a,b):
-    return np.sqrt(a**2+b**2)
-
-steps=200
+steps=100
+angles = []
 X=[]
 Y=[]
 sigma = 0.1
@@ -21,21 +22,31 @@ print("t,   x, y,    sigma,    succeeded")
 l = 50 # ile ostatnich mutacji wziac pod uwage
 last_l_mut = []
 apply_1_5 = True
+fits = []
+initial_intensity = 1e-5
+threshold_of_hearing = 1e-12
 
 for t in range(steps):
-    offspring = [x + sigma * random.gauss(0,1), y + sigma * random.gauss(0,1)]
-    if fitness(*offspring) < fitness(x, y):
-        x, y = offspring
+    offspring = angle + sigma * random.gauss(0, 1)
+    '''x + sigma * random.gauss(0,1),
+                 y + sigma * random.gauss(0,1)]'''
+    old = fitness(angle)
+    new = fitness(*offspring)
+    if new < old:
+        angle = offspring
         last_l_mut.append(True)
+        fits.append(new)
     else:
         last_l_mut.append(False)
+        fits.append(old)
 
     if len(last_l_mut) > l:
         last_l_mut.remove(last_l_mut[0])
-    X.append(x)
-    Y.append(y)
+    angles.append(angle)
+    #X.append(x)
+    #Y.append(y)
 
-    print t, np.round(x, 3),np.round(y, 3), sigma, succeeded
+    print t, np.round(angle, 3), sigma, succeeded
 
     if apply_1_5:
         succeeded = float(last_l_mut.count(True))
@@ -46,22 +57,15 @@ for t in range(steps):
         elif fi > 0.2:
             sigma = c_t * sigma
 
-plt.plot(range(steps), X, label='x')
-plt.scatter(range(steps), X)
+plt.plot(range(steps), angles)
+plt.scatter(range(steps), angles)
 plt.xlabel('czas')
-plt.ylabel('x / y')
-plt.plot(range(steps), Y, 'g', label='y')
-plt.scatter(range(steps), Y, color='g')
-plt.legend()
-plt.show(block=False)
+plt.ylabel('kat')
+plt.show()
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-ax.scatter(X, Y, fitness(np.array(X), np.array(Y)))
-ax.plot(X, Y, fitness(np.array(X), np.array(Y)))
-plt.xlabel('x')
-plt.xlabel('y')
+plt.plot(angles, fits, color='r')
+plt.scatter(angles, fits, color='r')
+plt.xlabel('kat')
 plt.ylabel('f dopasowania')
 plt.show()
 
